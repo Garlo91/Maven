fdf3be110e73ee6c1827f4628a9a8fb241845b33
unknown <git@github.com:Garlo91/Maven.git>
2019-10-26 21:38:48 -0500

    Initial Commit

---
+++ ProyectoBase/src/main/java/com/mx/get/ProyectoBase/App.java
@@ -0,0 +1,13 @@
+package com.mx.get.ProyectoBase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
